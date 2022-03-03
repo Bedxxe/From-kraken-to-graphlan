39bb85a284d699abe9c9ad295cfd5da1560691dd
Diego Garfias Gallegos <67386612+Bedxxe@users.noreply.github.com>
2022-03-02 23:57:38 -0600

    Update norm-rings.R

--- scripts/norm-rings.R
+++ scripts/norm-rings.R
@@ -12,6 +12,6 @@ write.table(ring1, file = "rings-files/tring1.txt", sep ="\t" ,
             row.names = FALSE, col.names = FALSE, quote = FALSE)
 
 ring2 <- read.table(file = "rings-files/ring2.txt")
-ring2$V4 <- range01(ring2$V4)
+ring2$V4 <- norm1(ring2$V4)
 write.table(ring2, file = "rings-files/tring2.txt", sep ="\t" ,
             row.names = FALSE, col.names = FALSE, quote = FALSE)
